438d7ce0b6aac919402fdc75713caf94e47d404e
Dr. Mohamad Amir Dliwati <mohamad.amir.dliwati@gmail.com>
2026-07-26 23:48:37 +0300

    fix: Update user model import in TelescopeServiceProvider

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\User;
+// use App\Models\User;
+use Webkul\User\Models\Admin as User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
