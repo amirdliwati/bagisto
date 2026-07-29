455114cf5f6f6559b6cbf483fb7842b9957be0d3
Dr. Mohamad Amir Dliwati <mohamad.amir.dliwati@gmail.com>
2026-07-29 19:36:52 +0300

    fix: update cart singleton to use CartFacade instead of Cart

--- packages/Webkul/Rewards/src/Providers/RewardsServiceProvider.php
+++ packages/Webkul/Rewards/src/Providers/RewardsServiceProvider.php
@@ -51,7 +51,7 @@ class RewardsServiceProvider extends ServiceProvider
         $loader->alias('cart', CartFacade::class);
 
         $this->app->singleton('cart', function () {
-            return new Cart;
+            return new CartFacade;
         });
 
         if (core()->getConfigData('reward.general.general.module-status')) {
